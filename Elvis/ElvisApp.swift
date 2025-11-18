b5603134b5c356dca0cf68b6cc70cc2fbaa65111
Matt D DiNovo <matt.d.dinovo@icloud.com>
2025-11-18 11:40:44 -0600

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Matthew DiNovo on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
